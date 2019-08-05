4aa9b1b3bac5655ac5428192babdc91e9870cfe4
Amir Hossein <samadipoor2@gmail.com>
2019-08-05 21:12:58 +0430

    Update IntorSlider.dart
    fix twice show bug

--- lib/pages/intro/IntorSlider.dart
+++ lib/pages/intro/IntorSlider.dart
@@ -5,38 +5,45 @@ import 'package:intro_slider/intro_slider.dart' as Intro;
 
 
 class IntroSlider extends StatelessWidget {
+
     Widget build(BuildContext context) {
         var data = SliderData();
         var pages = data.getPages();
-        return Scaffold(
-            body: SafeArea(
-                child: Intro.IntroSlider(
-                    slides: pages,
-                    renderNextBtn: Icon(
-                        Icons.navigate_next,
-                        color: Colors.black,
-                        size: 25.0,
-                    ),
-                    renderDoneBtn: Icon(
-                        Icons.done,
-                        color: Colors.black,
-                        size: 25.0,
-                    ),
-                    renderSkipBtn: Text(
-                        'SKIP',
-                        style: TextStyle(
+        return WillPopScope(
+            onWillPop: (){
+                return Future.value(false);
+            },
+            child: Scaffold(
+                body: SafeArea(
+                    child: Intro.IntroSlider(
+                        slides: pages,
+                        renderNextBtn: Icon(
+                            Icons.navigate_next,
                             color: Colors.black,
-                            fontSize: 14,
-                            fontWeight: FontWeight.bold,
+                            size: 25.0,
                         ),
+                        renderDoneBtn: Icon(
+                            Icons.done,
+                            color: Colors.black,
+                            size: 25.0,
+                        ),
+                        renderSkipBtn: Text(
+                            'SKIP',
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold,
+                            ),
+                        ),
+                        onDonePress: () async {
+                            await (await SharedPreferences.getInstance())
+                                    .setBool('INTRO_SEEN', true);
+//                            Navigator.pushReplacementNamed(context, '/');
+                            Navigator.pop(context);
+                        },
+                        backgroundColorAllSlides: Colors.grey[50],
+                        listCustomTabs: data.renderListCustomTabs(pages),
                     ),
-                    onDonePress: () async {
-                        await (await SharedPreferences.getInstance())
-                                .setBool('INTRO_SEEN', true);
-                        Navigator.pushReplacementNamed(context, '/');
-                    },
-                    backgroundColorAllSlides: Colors.grey[50],
-                    listCustomTabs: data.renderListCustomTabs(pages),
                 ),
             ),
         );
